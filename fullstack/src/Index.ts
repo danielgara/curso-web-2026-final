3b5c40ddd4203356e45b99569bc65eaac9b688bd
Daniel Correa <yo@danielgara.com>
2026-01-20 11:47:43 -0500

    first commit

---
+++ fullstack/src/Index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import type { Application } from 'express';
+import Routes from './routes/Routes.js';
+
+class Index {
+  static startServer(): void {
+    const app: Application = express();
+    const PORT = process.env.PORT || 3000;
+    
+    app.use(Routes.initializeRoutes());
+    
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  }
+}
+
+Index.startServer();
---
+++ fullstack/src/routes/Routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { HomeController } from '../controllers/HomeController.js';
+
+export default class Routes {
+  static initializeRoutes(): Router {
+    const router = Router();
+    
+    router.get('/', HomeController.index);
+    return router;
+  }
+}
