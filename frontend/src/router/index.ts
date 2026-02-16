53233de16f87ee996c99286a20287a71b5f98977
Daniel Correa <yo@danielgara.com>
2026-02-16 11:20:17 -0500

    imp: counter

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,6 +4,7 @@ import AboutView from '@/views/AboutView.vue';
 import BooksIndexView from '@/views/BooksIndexView.vue';
 import BooksShowView from '@/views/BooksShowView.vue';
 import BooksCreateView from '@/views/BooksCreateView.vue';
+import CounterView from '@/views/CounterView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,7 @@ const router = createRouter({
     { path: '/books', name: 'books', component: BooksIndexView, meta: { title: 'Books' } },
     { path: '/books/create', name: 'books.create', component: BooksCreateView, meta: { title: 'Create Book' } },
     { path: '/books/:id', name: 'book', component: BooksShowView, meta: { title: 'Book' } },
+    { path: '/counter', name: 'counter', component: CounterView, meta: { title: 'Counter' } },
   ],
 });
 
