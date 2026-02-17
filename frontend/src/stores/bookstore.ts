f20d0f63b4075c240b70c3d9f7105db4d374c742
Daniel Correa <yo@danielgara.com>
2026-02-17 11:06:27 -0500

    fix: composition api version

--- frontend/src/stores/bookstore.ts
+++ frontend/src/stores/bookstore.ts
@@ -1,8 +1,9 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 import type { BookInterface } from '@/interfaces/BookInterface.js';
 
-export const useBookStore = defineStore('book', {
-  state: () => ({
-    books: [] as BookInterface[],
-  }),
+export const useBookStore = defineStore('book', () => {
+  const books = ref<BookInterface[]>([]);
+
+  return { books, };
 });
